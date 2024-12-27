fbd706aff15961fc7c13fef94622d7dca1e72597
ecerentasci <ebrarcerentascii@gmail.com>
2024-12-27 16:39:10 +0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Sayfa yüklendikten sonra bu kod çalışacak
 document.addEventListener("DOMContentLoaded", function () {
-    const playButton = document.getElementById('play-music'); // Müzik başlatma butonu
-    const music = document.getElementById('background-music'); // Müzik dosyası
+    const playButton = document.getElementById('play-music'); 
+    const music = document.getElementById('background-music'); 
 
-    // Müzik açıldığında buton kaybolur ve müzik çalmaya başlar
+    
     playButton.addEventListener('click', function () {
         music.play();  // Müziği başlat
-        document.getElementById('music-overlay').style.display = 'none';  // Müzik açılış ekranını gizle
-        document.getElementById('main-content').style.display = 'block';  // Ana içeriği göster
+        document.getElementById('music-overlay').style.display = 'none';  
+        document.getElementById('main-content').style.display = 'block';  
     });
 });
